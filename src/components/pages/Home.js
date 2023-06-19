76e322816a360d4f4c817b833641a946b0d4f06d
soype <phenestroza@gmail.com>
2023-06-18 21:10:47 -0300

    Begin home design

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -39,7 +39,21 @@ const Home = (props) => {
         toggleAbout={aboutHandler}
         toggleContact={contactHandler}
       ></Header>
-      <div className={styles.home}>Home</div>
+      <div className={styles.home}>
+        <div className={styles.left}>
+          <h1>Discover the power of solar</h1>
+          <span className={styles['h-line']}></span>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi iusto, tempora animi maxime illum ex, optio aliquid nulla earum saepe debitis mollitia architecto? Odit, sit inventore ducimus tenetur nesciunt possimus.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae dolorum dicta libero est quibusdam voluptates natus, veniam optio numquam, exercitationem, autem culpa nesciunt rem fugit facilis atque dolor maxime delectus!</p>
+        </div>
+        <div className={styles.right}>
+          <span className={styles['v-line -animated']}></span>
+          <img src="" alt="" />
+          <span className={styles['h-line -animated']}></span>
+          <img src="" alt="" />
+        </div>
+        
+      </div>
     </div>
   );
 };
